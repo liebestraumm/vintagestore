83d0278d4fe0c3a367ba308834db4aec800d4a21
Carlos Agelvis <carlosarturo.agelvisvergara@connect.qut.edu.au>
2025-06-30 21:48:06 +1000

    Neon-Prisma serverless config

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,20 +1,14 @@
 "use server";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
-import { PrismaClient } from "../generated/prisma";
+import { prisma } from "@/db/prisma";
+import { convertToPlainObject } from "../utils";
 
 // Get the latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
-  // Serialize decimal fields (price and rating) before passing field's types to Zod
-  return data.map((product) => ({
-    ...product,
-    price: product.price.toFixed(2),
-    rating: product.rating.toFixed(1),
-  }));
+  return convertToPlainObject(data);
 }
