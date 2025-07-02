1d66f70ec48db4c1bcd42e340b437b65021151a0
Carlos Agelvis <carlosarturo.agelvisvergara@connect.qut.edu.au>
2025-07-02 13:38:09 +1000

    Fixing loader component to show up inside the root layout instead of entire screen for better UI

--- app/(root)/loading.tsx
+++ app/(root)/loading.tsx
@@ -3,7 +3,7 @@ import loader from "@/assets/loader.gif";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-full">
       <Image src={loader} width={150} height={150} alt="Loading..." />
     </div>
   );
