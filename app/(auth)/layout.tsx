22a7be71582251f4b5d0b438cc730841e21af0c5
Carlos Agelvis <carlosarturo.agelvisvergara@connect.qut.edu.au>
2025-09-07 20:21:51 +1000

    Implementing SignIn layout and page skeleton

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import { IChildrenNode } from "@/interfaces/IChildrenNode";
+import { FC } from "react";
+
+const Layout: FC<IChildrenNode> = ({ children }) => {
+  return <div className="flex-center min-h-screen w-full ">{children}</div>;
+};
+export default Layout;
